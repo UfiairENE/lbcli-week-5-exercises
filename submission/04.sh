8ff4c75789dc7190215091e3d4367f632bd36a08
UfiairENE <irenecufia@gmail.com>
2026-04-15 12:28:37 +0100

    Embedding Bitcoin Scripts in p2SH TXNs & Empowering Timelock with Bitcoin Scripts

--- submission/04.sh
+++ submission/04.sh
@@ -1,2 +1,15 @@
 # Create a CLTV script with a timestamp of 1495584032 and public key below:
 # publicKey=02e3af28965693b9ce1228f9d468149b831d6a0540b25e8a9900f71372c11fb277
+
+#!/bin/bash
+
+#!/bin/bash
+
+publicKey=02e3af28965693b9ce1228f9d468149b831d6a0540b25e8a9900f71372c11fb277
+timestamp=1495584032
+
+lehex=$(./integer2lehex.sh $timestamp | grep "Hexcode" | awk '{print $2}')
+
+pubKeyHash=$(echo -n $publicKey | xxd -r -p | openssl dgst -sha256 -binary | openssl dgst -rmd160 | awk '{print $2}')
+
+echo "${lehex}b17576a914${pubKeyHash}88ac"
--- submission/05.sh
+++ submission/05.sh
@@ -1,2 +1,14 @@
 # Create a CSV script that would lock funds until one hundred and fifty blocks had passed
 # publicKey=02e3af28965693b9ce1228f9d468149b831d6a0540b25e8a9900f71372c11fb277
+
+#!/bin/bash
+
+#!/bin/bash
+
+publicKey=02e3af28965693b9ce1228f9d468149b831d6a0540b25e8a9900f71372c11fb277
+
+lehex=$(./integer2lehex.sh 150 | grep "Hexcode" | awk '{print $2}')
+
+pubKeyHash=$(echo -n $publicKey | xxd -r -p | openssl dgst -sha256 -binary | openssl dgst -rmd160 | awk '{print $2}')
+
+echo "${lehex}b27576a914${pubKeyHash}88ac"
